a1d6fd15afd3a7a358ca7ae49278537ba64833d7
Jordan Ndetcho <jordan.ndetcho.ext@suez.com>
2024-03-11 14:10:13 +0100

    update database config

--- strapi-dcl-ksp/config/env/production/server.js
+++ strapi-dcl-ksp/config/env/production/server.js
@@ -1,13 +1,11 @@
 // Path: ./config/env/production/server.js
 // starting from Strapi v 4.6.1 server.js has to be the following
-const path = require('path');
-require('dotenv').config({ path: path.resolve(__dirname, '.env') });
 
 module.exports = ({ env }) => ({
     proxy: true,
     host: "0.0.0.0",
     port: process.env.PORT,
-    url: env('MY_HEROKU_URL'),
+    url: env('DATABASE_HEROKU_URL'),
     app: {
         keys: env.array('APP_KEYS')
     },
--- strapi-dcl-ksp/config/server.js
+++ strapi-dcl-ksp/config/server.js
@@ -1,10 +1,19 @@
+// Path: ./config/env/production/server.js
+// starting from Strapi v 4.6.1 server.js has to be the following
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '.env') });
+
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
-  },
-  webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
-  },
-});
+    proxy: true,
+    host: "0.0.0.0",
+    port: process.env.PORT,
+    url: env('MY_HEROKU_URL'),
+    app: {
+        keys: env.array('APP_KEYS')
+    },
+    admin: {
+        auth: {
+            secret: env('ADMIN_JWT_SECRET'),
+        },
+    },
+})
